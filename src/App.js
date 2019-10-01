3c4172248d2083a4ed8618bff73f853e83192882
Sterling Cobb <sterling@inkarnate.com>
2019-09-30 23:36:12 -0600

    refactor fetchUrl out of the useFetch effect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 import './App.css';
+
+
+const fetchUrl = async (url, onSuccess, onError) => {
+  try{
+    const response = await fetch(url)
+    const json = await response.json()
+
+    onSuccess(json)
+  } catch (e) {
+    onError(e)
+  }
+}
 
 const useFetch = (url) => {
   const [responseData, setResponseData] = useState([])
   const [loading, setLoading] = useState(true)
 
-   const fetchUrl = async () => {
-     try{
-      const response = await fetch(url)
-      const json = await response.json()
+  const onSuccess = (json) => {
+    setResponseData(json)
+    setLoading(false)
+  }
 
-      setResponseData(json)
-      setLoading(false)
-     } catch (e) {
-      setLoading(false)
-      console.log("You dun broke somethin", e)
-     }
+  const onError = (error) => {
+    setLoading(false)
+    console.log("You dun broke somethin", error)
   }
 
   useEffect(() => {
-    fetchUrl()
+    fetchUrl(url, onSuccess, onError)
   }, [url]);
 
   return [responseData, loading]
@@ -33,23 +42,55 @@ const WithLoading = ({loading, children}) => {
   }
 }
 
+const MovieList = ({listTitle, movies}) => {
+  if (movies)  {
+    return(
+      <div>
+        <ol>
+          {movies.results && movies.results.length && movies.results.map(movie => 
+            <li key={movie.id}>
+              <h2>{movie.title}</h2>
+            </li>)}
+        </ol>
+      </div>
+    )
+  } else {
+    return null
+  }
+}
+
+const PopularMovieList = ({movies}) => {
+  return(
+    <div>
+      <h2>Popular Movies</h2>
+      <MovieList movies={movies} />
+    </div>
+  )
+}
+
+const SearchResults = ({movies}) => {
+  return(
+    <div>
+      <h2>Search Results</h2>
+      <MovieList movies={movies} />
+    </div>
+  )
+}
+
 const App = () => {
-  const [movies, loading] = useFetch('movie/popular')
+  const [popularMovies, popularMoviesLoading] = useFetch('movie/popular')
+  const [query, setQuery] = useState('')
+
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie Search</h1>
+        <input type="search" onChange={(e) => setQuery(e.target.value)}/>
       </header>
-      <div>
-        <WithLoading loading={loading}>
-          <ol>
-            {movies.results && movies.results.length && movies.results.map(movie => <li key={movie.id}>
-              <h2>{movie.title}</h2>
-            </li>)}
-          </ol>
-        </WithLoading>
-      </div>
+      <WithLoading loading={popularMoviesLoading}>
+        {query.trim().length ? <div>search results</div> : <PopularMovieList movies={popularMovies} />}
+      </WithLoading>
     </div>
   );
 }
